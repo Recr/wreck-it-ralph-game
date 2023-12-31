f8f51c1848e21a9f5a3534ca6c6fd2ad1a466619
Recr <126016150+Recr@users.noreply.github.com>
2023-12-31 13:41:54 -0300

    game

---
+++ src/scripts/engine.js
@@ -0,0 +1,55 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        hitPosition: 0,
+        result: 0,
+        currentTime: 20,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 500),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0){
+        alert("Times up!\n Your score: " + state.values.result);
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) =>{
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () =>{
+            if(square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
+    });
+}
+
+function main() {
+    addListenerHitBox();
+}
+
+main();
